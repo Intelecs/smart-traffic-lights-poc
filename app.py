2785c5bfbea42abd2383369e8a83d516c7125b9d
JanuaryThomas <january_thomas@hotmail.com>
2022-05-12 16:25:09 +0300

    adding bluetooth client

--- app.py
+++ app.py
@@ -4,4 +4,4 @@ import asyncio
 
 if __name__ == "__main__":
     bluetooth_client = BluetoothClient()
-    asyncio.run(bluetooth_client.get_mac_address())
+    bluetooth_client.get_mac_address()
--- device/BluetoothClient.py
+++ device/BluetoothClient.py
@@ -1,7 +1,6 @@
 import bluetooth
 from dataclasses import dataclass
 from utils.utils import get_logger
-from bleak import BleakScanner
 
 
 
@@ -10,13 +9,9 @@ class BluetoothClient:
     logger = get_logger(name=__name__)
     is_ble: bool = False
 
-    async def get_mac_address(self) -> str:
-        devices = await BleakScanner.discover()
-        for device in devices:
-            print(await device)
-            
-        # mac_address = bluetooth.read_local_bdaddr()
-        # logger.info("Mac address: %s", mac_address)
+    def get_mac_address(self) -> bool:
+        mac_address = bluetooth.read_local_bdaddr()
+        self.logger.info("Mac address: %s", mac_address)
         # return mac_address
     
     def start_bluetooth(self) -> None:
