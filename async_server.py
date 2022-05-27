cb23773a61822aaa93702ef86f04343dcdd66376
JanuaryThomas <january_thomas@hotmail.com>
2022-05-27 22:21:35 +0300

    adding

--- async_server.py
+++ async_server.py
@@ -16,9 +16,9 @@ app = Starlette(debug=True)
 
 mqtt_client = MQTTclient(
     host="axwsbhnc43ml1-ats.iot.eu-west-1.amazonaws.com",
-    root_ca_path="greengrass/greengrass-v2-certs/AmazonRootCA1.pem",
-    private_key_path="greengrass/greengrass-v2-certs/JunctionNodeA/7be9635fcee9f1b70a0f2f0e33f2468703e849d8388d73cce9d1c29b5fa77124-private.pem.key",
-    certificate_path="greengrass/greengrass-v2-certs/JunctionNodeA/7be9635fcee9f1b70a0f2f0e33f2468703e849d8388d73cce9d1c29b5fa77124-certificate.pem.crt",
+    root_ca_path="certs/AmazonRootCA1.pem",
+    private_key_path="certs/JunctionNodeA/7be9635fcee9f1b70a0f2f0e33f2468703e849d8388d73cce9d1c29b5fa77124-private.pem.key",
+    certificate_path="certs/JunctionNodeA/7be9635fcee9f1b70a0f2f0e33f2468703e849d8388d73cce9d1c29b5fa77124-certificate.pem.crt",
 )
 
 mqtt_client.__setup__()
