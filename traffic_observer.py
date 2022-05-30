169e6343493a533b687884b5d797c8c93827735e
JanuaryThomas <january_thomas@hotmail.com>
2022-05-30 08:18:48 +0300

    adding cv2

--- traffic_observer.py
+++ traffic_observer.py
@@ -81,7 +81,8 @@ if __name__ == '__main__':
         conf["model_path"])
     net.setPreferableTarget(cv2.dnn.DNN_TARGET_CPU)
 
-    stream = VideoGear(source=0, logging=True).start()
+    # stream = VideoGear(source=0, logging=True).start()
+    stream = cv2.VideoCapture(0)
 
     H = None
     W = None
