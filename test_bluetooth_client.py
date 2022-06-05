416b00a30921de3a23b383a1528fb314924c83a4
JanuaryThomas <january_thomas@hotmail.com>
2022-06-05 18:05:29 +0300

    bluetooth

--- test_bluetooth_client.py
+++ test_bluetooth_client.py
@@ -2,5 +2,7 @@ from device.BluetoothClient import BluetoothClient
 
 
 bluetooth_client = BluetoothClient()
-
+nearby_devices = bluetooth_client.nearby_devices()
+print(nearby_devices)
 bluetooth_client.rfcom_client("Hello")
+
