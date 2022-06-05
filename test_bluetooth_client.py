8406b08374341f82b496ace7cee8bed91c25a651
JanuaryThomas <january_thomas@hotmail.com>
2022-06-05 18:08:02 +0300

    bluetooth

--- test_bluetooth_client.py
+++ test_bluetooth_client.py
@@ -4,5 +4,8 @@ from device.BluetoothClient import BluetoothClient
 bluetooth_client = BluetoothClient()
 nearby_devices = bluetooth_client.nearby_devices()
 print(nearby_devices)
+
+mac_address = bluetooth_client.get_mac_address()
+print(mac_address)
 bluetooth_client.rfcom_client("Hello")
 
--- test_bluetooth_server.py
+++ test_bluetooth_server.py
@@ -3,4 +3,10 @@ from device.BluetoothClient import BluetoothClient
 
 bluetooth_client = BluetoothClient()
 
+nearby_devices = bluetooth_client.nearby_devices()
+print(nearby_devices)
+
+mac_address = bluetooth_client.get_mac_address()
+print(mac_address)
+
 bluetooth_client.rfcom_server()
