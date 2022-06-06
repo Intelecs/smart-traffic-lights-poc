700bfa3b3e36a1093ea3f1eb3356cf5bdfd3bf97
JanuaryThomas <january_thomas@hotmail.com>
2022-06-06 14:24:21 +0300

    updating ip

--- test_node_to_master.py
+++ test_node_to_master.py
@@ -6,28 +6,33 @@ import socket
 logger = get_logger(name=__name__)
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.connect(("8.8.8.8", 80))
+sock.connect(("8.8.8.8", 8000))
 local_ip = sock.getsockname()[0]
-sock.close()
+# sock.close()
+
+remote_ip = sock.gethostbyname('raspberrypi')
+print(remote_ip)
 
 
-async def violation_api(image):
-    url = f"http://{local_ip}:8000/violations"
-    headers = {'Content-Type': 'application/json'}
-    data = {
-        "right": image
-    }
-    try:
-        requests.post(url, json=data, headers=headers)
-        logger.info(f"Violation sent to {url}")
-    except Exception as e:
-        logger.error(f"Error sending violation to {url}")
-        logger.error(e)
 
-async def send_signal(image):
-    asyncio.ensure_future(violation_api(image))
-    await asyncio.sleep(0)
 
-if __name__ == '__main__':
-    while True:
-        asyncio.run(send_signal("RED"))
+# async def violation_api(image):
+#     url = f"http://{local_ip}:8000/violations"
+#     headers = {'Content-Type': 'application/json'}
+#     data = {
+#         "right": image
+#     }
+#     try:
+#         requests.post(url, json=data, headers=headers)
+#         logger.info(f"Violation sent to {url}")
+#     except Exception as e:
+#         logger.error(f"Error sending violation to {url}")
+#         logger.error(e)
+
+# async def send_signal(image):
+#     asyncio.ensure_future(violation_api(image))
+#     await asyncio.sleep(0)
+
+# if __name__ == '__main__':
+#     while True:
+#         asyncio.run(send_signal("RED"))
