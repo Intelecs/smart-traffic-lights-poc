2cb90c868ee32965bd463c6fd311c0dbc55f18de
JanuaryThomas <january_thomas@hotmail.com>
2022-07-05 17:03:42 +0300

    adding duty cylue

--- tes_normal_state.py
+++ tes_normal_state.py
@@ -27,24 +27,24 @@ except Exception as e:
     logger.error(f"Not running on Raspberry Pi {e}")
     is_raspberry = False
 
-try:
-    logger.info("Starting Traffic Lights threading...")
-    while True:
-        if GPIO.input(BUTTON) == GPIO.HIGH:
-            print('Button pressed')
-            logger.info("Sending SIGNALS To other juction")
-            counter = 0
-            while counter < 61:
-                logger.info(f"count {counter}")
-                if counter == 60:
-                    pass
-                    # traffic_state(1, 0, 0)  # stop the vehicles
+# try:
+#     logger.info("Starting Traffic Lights threading...")
+#     while True:
+#         if GPIO.input(BUTTON) == GPIO.HIGH:
+#             print('Button pressed')
+#             logger.info("Sending SIGNALS To other juction")
+#             counter = 0
+#             while counter < 61:
+#                 logger.info(f"count {counter}")
+#                 if counter == 60:
+#                     pass
+#                     # traffic_state(1, 0, 0)  # stop the vehicles
 
-                    # traffic_light_pedestrian()
-                time.sleep(1)
-                counter += 1
-        # traffic_normal()
-        ped_traffic_state(1, 0, 0)
-except KeyboardInterrupt as e:
-    logger.error("Something went wrong with traffic lights {}".format(e), exc_info=True)
-    GPIO.cleanup()
+#                     # traffic_light_pedestrian()
+#                 time.sleep(1)
+#                 counter += 1
+#         # traffic_normal()
+#         ped_traffic_state(1, 0, 0)
+# except KeyboardInterrupt as e:
+#     logger.error("Something went wrong with traffic lights {}".format(e), exc_info=True)
+#     GPIO.cleanup()
